b402d1de8696281f4eb975aea2d2593c0739e555
Aleksandr22822 <alek5andr.bubnov@yandex.ru>
2024-05-22 21:05:44 +0300

    Добавьте файлы проекта.

---
+++ Task 13.6.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Collections;
+using System.Runtime.Intrinsics.Wasm;
+using System.Linq;
+namespace Generic
+{
+    class Gen
+    {
+        static void Main()
+        {
+            Dictionary<string, int> stringes = new Dictionary<string, int>();
+            string text = File.ReadAllText("C:\\SkillFactory\\Res\\cdev_Texte.txt");
+            string TextUpper = text.ToUpper();
+            var noPunctuationText = new string(TextUpper.Where(c => !char.IsPunctuation(c)).ToArray());
+            string ars = noPunctuationText.ToString();
+            string[] TextMassiv = ars.Split();
+            HashSet<string> UnicString = new HashSet<string>(TextMassiv);
+
+            foreach (var s in UnicString)
+            {
+                stringes.Add(s, 0);
+            }
+
+            foreach (var s in stringes.Keys)
+            {
+                foreach (var p in TextMassiv)
+                {
+                    if (s == p)
+                    {
+                        stringes[s] += 1;
+                    }
+                }
+              
+            }
+            Console.WriteLine($"Десять самых часто встречающихся слов");
+
+            foreach (var s in stringes.OrderByDescending(c => c.Value))
+            {
+                Console.WriteLine($"{s.Key} - {s.Value}");
+            }
+        }
+    }
+}
